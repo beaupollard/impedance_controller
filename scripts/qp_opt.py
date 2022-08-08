c00a0278cb2d09917f0fffb67d9fbf10e73f7934
Beau Pollard <beauppollard@gmail.com>
2022-08-08 19:03:26 -0400

    Changed Lambda calculation to avoid inverting Jacobians

--- scripts/qp_opt.py
+++ scripts/qp_opt.py
@@ -1,3 +1,4 @@
+from turtle import pos
 import numpy as np
 import mujoco_py
 from scipy.spatial.transform import Rotation as R
@@ -8,6 +9,8 @@ class qp_opt():
     def __init__(self,sim):
         self.sim=sim
         self.ee_site='mep:ee'
+        self.target_site='wpt1'
+        self.qvel_index=np.array([0,1,2,3,4,5])
         self.vdot = cp.Variable(len(self.sim.data.qacc[:6]))
         self.pos_lower_lims=self.sim.model.jnt_range[:6,0]
         self.pos_upper_lims=self.sim.model.jnt_range[:6,1]
@@ -16,16 +19,21 @@ class qp_opt():
         Jin=np.vstack((np.reshape(self.sim.data.get_site_jacp(self.ee_site)[:],(3,-1)),np.reshape(self.sim.data.get_site_jacr(self.ee_site)[:],(3,-1))))
         self.J_prev=Jin[:6,:6]
         self.dt=sim.model.opt.timestep
+        self.kp=np.array([120,120,120,120,120,120])
+        self.kd=np.array([60,60,60,60,60,60])
     
-    def run_opt(self,qacc):
+    def run_opt(self):
 
 
         ## Get Jacobian of end effector ##
-        Jin=np.vstack((np.reshape(self.sim.data.get_site_jacp(self.ee_site)[:],(3,-1)),np.reshape(self.sim.data.get_site_jacr(self.ee_site)[:],(3,-1))))
-        J=Jin[:6,:6]
+        J_ori=np.array(self.sim.data.get_site_jacr(self.ee_site).reshape((3, -1))[:, self.qvel_index])     
+        J_pos=np.array(self.sim.data.get_site_jacp(self.ee_site).reshape((3, -1))[:, self.qvel_index])  
+        J=np.array(np.vstack([J_pos, J_ori]))     
+        Jtinv = np.linalg.pinv(np.transpose(J))
+        Jinv = np.linalg.pinv(J)
 
         ## Get bias terms (coriollis/gravity) ##
-        h=self.sim.data.qfrc_bias[:6]
+        fbias=self.sim.data.qfrc_bias[:6]
 
         ## Get full mass matrix ##
         rc=len(self.sim.data.qvel)
@@ -38,9 +46,46 @@ class qp_opt():
 
         self.J_prev=np.array(J)
 
+        ## Me = J^-T M J^-1
+        lambda_ori=np.linalg.pinv(J_ori@np.linalg.inv(mass_matrix)@J_ori.T)
+        lambda_pos=np.linalg.pinv(J_pos@np.linalg.inv(mass_matrix)@J_pos.T)        
+        Me = Jtinv@mass_matrix@Jinv
+
+        ## Ce ##
+        Ce = Jtinv@mass_matrix@Jinv@dJdt@Jinv
+
+        current_pos=self.sim.data.get_site_xpos(self.ee_site)
+        desired_pos=self.sim.data.get_site_xpos(self.target_site)
+        current_velp=self.sim.data.get_site_xvelp(self.ee_site)
+
+        current_ori=self.sim.data.get_site_xmat(self.ee_site)
+        desired_ori=self.sim.data.get_site_xmat(self.target_site)
+        current_velr=self.sim.data.get_site_xvelr(self.ee_site)
+        
+        q_err=self.orientation_err(desired_ori,current_ori)
+
+        position_err=np.concatenate((desired_pos-current_pos,q_err))
+        velocity_err=-np.concatenate((current_velp,current_velr))
+
+        Xdes=np.diag(self.kp)@position_err+np.diag(self.kd)@velocity_err
+
+        desired_wrench=np.concatenate((lambda_pos@Xdes[:3],lambda_ori@Xdes[3:]))
+        # tau=np.transpose(J)@(desired_wrench+Ce@(velocity_err))+fbias
+        tau=np.transpose(J)@(desired_wrench)+fbias
+        
+        for i in range(6):
+            self.sim.data.ctrl[i]=tau[i]
+
+        ## QP Optimizer ##
         qdot_prev=self.sim.data.qvel[:6]
         q_prev=self.sim.data.qpos[:6]
-        
+        ## Set the calculated torques
+        # for i in range(6):
+        #     self.sim.data.ctrl[i]=tau[i]
+
+        ## Determine the joint accelerations ##
+        self.sim.forward()     
+
         # Construct the problem.
         constraints=[]
         vdot = cp.Variable(len(self.sim.data.qacc[:6]))
@@ -68,3 +113,14 @@ class qp_opt():
             self.sim.data.ctrl[i]=self.sim.data.qfrc_inverse[i]
         return vdot.value
 
+    def orientation_err(self,desired,current):
+        rc1 = current[0:3, 0]
+        rc2 = current[0:3, 1]
+        rc3 = current[0:3, 2]
+        rd1 = desired[0:3, 0]
+        rd2 = desired[0:3, 1]
+        rd3 = desired[0:3, 2]
+
+        error = 0.5 * (np.cross(rc1, rd1) + np.cross(rc2, rd2) + np.cross(rc3, rd3))
+
+        return error
--- scripts/runner.py
+++ scripts/runner.py
@@ -15,40 +15,23 @@ view=True
 if view==True:
     viewer=MjViewer(mj_sim.sim)
 
-## Setup the controller ##
-ctrl=osc(sim=mj_sim.sim,eef_name='mep:ee',joint_indexes=mj_sim.joint_index,actuator_range=(-100*np.ones(6),100*np.ones(6)))
-ctrl.kp=120*np.ones(6)
-ctrl.kd=60.*np.ones(6)
-ctrl.model_timestep=mj_sim.sim.model.opt.timestep
-constraints=[]
-dt=mj_sim.sim.model.opt.timestep
+## Setup Controller ##
 qp=qp_opt(mj_sim.sim)
+
 q_opt=[]
 tau_0=[]
 while mj_sim.sim.data.time<10:
-    r=R.from_matrix(mj_sim.sim.data.get_body_xmat('satellite'))
-    r2=R.from_matrix(mj_sim.sim.data.get_body_xmat('antenna'))
-    ctrl.goal_pos=mj_sim.sim.data.get_site_xpos('wpt1')
-    ctrl.goal_ori=r2.as_matrix()@r.as_matrix()
 
-    t0=mj_sim.sim.data.time
-    tau=ctrl.run_controller()
-    
-    ## Set the calculated torques
-    for i in range(6):
-        mj_sim.sim.data.ctrl[i]=tau[i]
+    # ## Run the QP optimizer ##
+    q_out=qp.run_opt()
 
-    ## Determine the joint accelerations ##
-    mj_sim.sim.forward()
-
-    ## Run the QP optimizer ##
-    q_out=qp.run_opt(mj_sim.sim.data.qacc)
-    q_opt.append([pos for pos in q_out])
-    tau_0.append([pos for pos in tau])
+    # q_out=qp.run_opt(mj_sim.sim.data.qacc)
+    # q_opt.append([pos for pos in mj_sim.sim.data.qvel[:6]])
+    # tau_0.append([pos for pos in tau])
 
     ## Step Mujoco Forward ##
     mj_sim.sim.step()
-    mj_sim.sim.data.qpos[:6]
+
     if view==True:
         viewer.render()
 
