89f2939bb2f202a3042d44136886a3c4a9f56c4f
Beau Pollard <beauppollard@gmail.com>
2022-08-09 12:50:19 -0400

    Changed omptimization function to try to match tau instead of qacc

--- scripts/qp_opt.py
+++ scripts/qp_opt.py
@@ -10,6 +10,10 @@ class qp_opt():
         self.sim=sim
         self.ee_site='mep:ee'
         self.target_site='wpt1'
+        self.pegmass=self.sim.model.body_mass[self.sim.model.body_name2id('peg')]
+        self.acc_index=self.sim.model.sensor_adr[self.sim.model.sensor_name2id('acc')]
+        self.force_index=self.sim.model.sensor_adr[self.sim.model.sensor_name2id('force_sensor')]
+        self.torque_index=self.sim.model.sensor_adr[self.sim.model.sensor_name2id('torque_sensor')]
         self.qvel_index=np.array([0,1,2,3,4,5])
         self.vdot = cp.Variable(len(self.sim.data.qacc[:6]))
         self.pos_lower_lims=self.sim.model.jnt_range[:6,0]
@@ -21,6 +25,7 @@ class qp_opt():
         self.dt=sim.model.opt.timestep
         self.kp=np.array([120,120,120,120,120,120])
         self.kd=np.array([60,60,60,60,60,60])
+
     
     def run_opt(self):
 
@@ -70,47 +75,34 @@ class qp_opt():
         Xdes=np.diag(self.kp)@position_err+np.diag(self.kd)@velocity_err
 
         desired_wrench=np.concatenate((lambda_pos@Xdes[:3],lambda_ori@Xdes[3:]))
-        # tau=np.transpose(J)@(desired_wrench+Ce@(velocity_err))+fbias
         tau=np.transpose(J)@(desired_wrench)+fbias
-        
-        for i in range(6):
-            self.sim.data.ctrl[i]=tau[i]
 
         ## QP Optimizer ##
         qdot_prev=self.sim.data.qvel[:6]
         q_prev=self.sim.data.qpos[:6]
-        ## Set the calculated torques
-        # for i in range(6):
-        #     self.sim.data.ctrl[i]=tau[i]
-
-        ## Determine the joint accelerations ##
-        self.sim.forward()     
 
         # Construct the problem.
         constraints=[]
         vdot = cp.Variable(len(self.sim.data.qacc[:6]))
+        u = cp.Variable(len(self.sim.data.qacc[:6]))
         pos_lower_lims=self.sim.model.jnt_range[:6,0]
         pos_upper_lims=self.sim.model.jnt_range[:6,1]
-        vel_lower_lims=-math.pi*np.ones(6)
-        vel_upper_lims=math.pi*np.ones(6)
-        objective = cp.Minimize(cp.sum_squares(vdot-self.sim.data.qacc[:6]))
+        vel_lower_lims=-math.pi/4*np.ones(6)
+        vel_upper_lims=math.pi/4*np.ones(6)
 
+        objective = cp.Minimize(cp.sum_squares(u-tau))
+        constraints.append(mass_matrix@vdot+fbias==u+self.endeffector_force())
+        constraints.append(vel_lower_lims <= vdot*self.dt+qdot_prev)
         constraints.append(vel_lower_lims <= vdot*self.dt+qdot_prev)
         constraints.append(vel_upper_lims >= vdot*self.dt+qdot_prev)
         constraints.append(pos_lower_lims <= vdot*self.dt**2+qdot_prev*self.dt+q_prev)
         constraints.append(pos_upper_lims >= vdot*self.dt**2+qdot_prev*self.dt+q_prev)        
         prob = cp.Problem(objective, constraints)
 
-        result = prob.solve()
+        result = prob.solve(verbose=True)
 
-
-        ## Set the calculated torques
         for i in range(6):
-            self.sim.data.qacc[i]=vdot.value[i]      
-
-        mujoco_py.cymj._mj_inverse(self.sim.model,self.sim.data)
-        for i in range(6):
-            self.sim.data.ctrl[i]=self.sim.data.qfrc_inverse[i]
+            self.sim.data.ctrl[i]=u.value[i]
         return vdot.value
 
     def orientation_err(self,desired,current):
@@ -124,3 +116,11 @@ class qp_opt():
         error = 0.5 * (np.cross(rc1, rd1) + np.cross(rc2, rd2) + np.cross(rc3, rd3))
 
         return error
+
+    def endeffector_force(self):
+        bias=self.pegmass*self.sim.data.sensordata[self.acc_index:self.acc_index+3]
+        Fx=self.sim.data.sensordata[self.force_index:self.force_index+3]-bias
+        Tx=self.sim.data.sensordata[self.torque_index:self.torque_index+3]
+        R=self.sim.data.get_site_xmat(self.ee_site)
+        Fext=-np.concatenate((np.linalg.inv(R)@Fx,np.linalg.inv(R)@Tx))
+        return Fext
--- scripts/runner.py
+++ scripts/runner.py
@@ -7,6 +7,7 @@ from mujoco_node import mj_node
 from scipy.spatial.transform import Rotation as R
 from robosuite.controllers.osc import OperationalSpaceController as osc
 from qp_opt import qp_opt
+import math
 
 ## Setup Mujoco Sim Node ##
 mj_sim=mj_node()
@@ -25,15 +26,14 @@ while mj_sim.sim.data.time<10:
     # ## Run the QP optimizer ##
     q_out=qp.run_opt()
 
-    # q_out=qp.run_opt(mj_sim.sim.data.qacc)
-    # q_opt.append([pos for pos in mj_sim.sim.data.qvel[:6]])
-    # tau_0.append([pos for pos in tau])
-
     ## Step Mujoco Forward ##
     mj_sim.sim.step()
+
+    q_opt.append([pos for pos in mj_sim.sim.data.qvel[:6]])
+    # tau_0.append([pos for pos in tau])
 
     if view==True:
         viewer.render()
 
 t0=np.array(tau_0)
-q0=np.array(q_opt)
+q0=np.array(q_opt)*180/math.pi
